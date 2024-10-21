6057e6b8b705f9e2d023d72c3985913353e28e0a
hanane75 <benkada.hanane@gmail.com>
2024-10-21 15:09:08 +0200

    correction formulaire de contact

--- salhi.hanane/src/components/Contact.jsx
+++ salhi.hanane/src/components/Contact.jsx
@@ -1,7 +1,8 @@
-import emailjs from 'emailjs-com';
+import axios from 'axios';
 import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap'; // Importation des composants Modal et Button de Bootstrap
 
-const ContactForm = () => {
+const Contact = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -9,6 +10,7 @@ const ContactForm = () => {
   });
 
   const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -18,70 +20,101 @@ const ContactForm = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Passer directement formData au lieu de e.target pour éviter l'utilisation de 'name' EmailJS
-    emailjs.send('service_2s9f0r4', 'template_qfjwxun', formData, 'nMrEO-9ZdDyaFE6Ra')
-    .then((result) => {
-      console.log('Message envoyé avec succès !', result.text);
-      setSubmitted(true); // Affiche un message de succès
-    }, (error) => {
-      console.log('Erreur lors de l\'envoi du message :', error.text);
-    });
+    try {
+      // Remplacez l'URL par celle obtenue dans Getform
+      const response = await axios.post('https://getform.io/f/bjjeknob', formData);
+
+      if (response.status === 200) {
+        setSubmitted(true); // Ouvre la modal
+        setError(null); // Réinitialise les erreurs
+
+        // Réinitialiser les champs du formulaire
+        setFormData({
+          name: '',
+          email: '',
+          message: '',
+        });
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'envoi du message :', error);
+      setError('Erreur lors de l\'envoi du message. Veuillez réessayer.');
+    }
+  };
+
+  const handleCloseModal = () => {
+    setSubmitted(false); // Fermer la modal
   };
 
   return (
     <section id='contact' className="contact-section padding-section">
       <h2>{"< Contactez-moi >"}</h2>
-      {submitted ? (
-        <div className="thank-you-message">
-          <p>Merci pour votre message. Je vous contacterai bientôt.</p>
+      
+      {/* Formulaire */}
+      <form onSubmit={handleSubmit} className="contact-form">
+        <div className="form-group">
+          <label htmlFor="name">Nom</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-control"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </div>
-      ) : (
-        <form onSubmit={handleSubmit} className="contact-form">
-          <div className="form-group">
-            <label htmlFor="name">Nom</label>
-            <input
-              type="text"
-              id="name"
-              name="name" // Alignez avec le formData "name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
 
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email" // Alignez avec le formData "email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-control"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-          <div className="form-group">
-            <label htmlFor="message">Message</label>
-            <textarea
-              id="message"
-              name="message" // Alignez avec le formData "message"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            />
-          </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            className="form-control"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-          <button type="submit" className="contact-submit-button">
-            Envoyer
-          </button>
-        </form>
-      )}
+        {error && <p className="text-danger">{error}</p>} {/* Gestion des erreurs */}
+
+        <button type="submit" className="btn btn-primary">
+          Envoyer
+        </button>
+      </form>
+
+      {/* Modal Bootstrap */}
+      <Modal show={submitted} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Merci !</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Merci pour votre message. Je vous contacterai bientôt.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Fermer
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </section>
   );
 };
 
-export default ContactForm;
+export default Contact;
--- salhi.hanane/src/components/ProjectsComponent.jsx
+++ salhi.hanane/src/components/ProjectsComponent.jsx
@@ -1,67 +1,140 @@
-import React, { useState, useEffect } from 'react';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import projectsData from '../Data/projectsData.json';
-import { FaGithub} from "react-icons/fa";
-const ProjectsComponent = () => {
-  const [projects, setProjects] = useState([]);
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap'; // Importation des composants Modal et Button de Bootstrap
 
-  useEffect(() => {
-    // Charger les projets depuis le fichier JSON
-    setProjects(projectsData);
-  }, []);
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
 
-  // Configurer les options de responsive design pour le carrousel
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 1024 },
-      items: 2, // 3 éléments pour grands écrans
-    },
-    desktop: {
-      breakpoint: { max: 1024, min: 768 },
-      items: 2, // 2 éléments pour desktop
-    },
-    tablet: {
-      breakpoint: { max: 768, min: 464 },
-      items: 1, // 1 élément pour tablettes
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1, // 1 élément pour mobile
-    },
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      
+      const response = await axios.post('https://getform.io/f/bjjeknob', formData);
+
+      if (response.status === 200) {
+        setSubmitted(true); // Ouvre la modal
+        setError(null); // Réinitialise les erreurs
+
+        // Réinitialiser les champs du formulaire
+        setFormData({
+          name: '',
+          email: '',
+          message: '',
+        });
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'envoi du message :', error);
+      setError('Erreur lors de l\'envoi du message. Veuillez réessayer.');
+    }
+  };
+
+  const handleCloseModal = () => {
+    setSubmitted(false); // Fermer la modal
   };
 
   return (
-    <section  id='Projects' className="skills-projects-section">
-      <section className="projects-section padding-section">
-        <h2 className="skills-title">{"< projets >"}</h2>
-
-        <div className="projects-content">
-          <p>
-            Explorez les projets web innovants réalisés par SALHI Hanane, une développeuse web passionnée basée à Paris.
-            Avec un œil avisé pour le design moderne et la fonctionnalité, je concrétise vos idées numériques. Mon 
-            portfolio présente une gamme de projets achevés, mettant en avant mes compétences techniques et mon engagement 
-            pour la qualité. Interagissez avec des éléments interactifs et des animations qui reflètent ma dévotion à créer 
-            des expériences web impactantes.
-          </p>
+    <section id='contact' className="contact-section padding-section">
+      <h2>{"< Contactez-moi >"}</h2>
+      
+      <div className="row">
+        {/* Colonne gauche pour les informations de contact */}
+        <div className="col-md-6 contact-info">
+          <div className="info-box">
+            <i className="fas fa-phone"></i>
+            <p>+48 012 123 123</p>
+          </div>
+          <div className="info-box">
+            <i className="fas fa-envelope"></i>
+            <p>email@example.com</p>
+          </div>
+          <div className="info-box">
+            <i className="fas fa-map-marker-alt"></i>
+            <p>Chittagong, Bangladesh</p>
+          </div>
         </div>
 
-        {/* Carrousel pour afficher plusieurs projets à la fois */}
-        <Carousel responsive={responsive} infinite={true} autoPlay={true} autoPlaySpeed={3000} keyBoardControl={true}>
-          {projects.map((project) => (
-            <div key={project.id} className="project-card">
-              <img src={project.image} alt={project.title} className="project-image" />
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              <a href={project.url} className="project-link"><FaGithub size={20} color="#333" />
-              {" Voir demo >>>"}
-              </a>
+        {/* Colonne droite pour le formulaire */}
+        <div className="col-md-6">
+          <form onSubmit={handleSubmit} className="contact-form">
+            <div className="form-group">
+              <label htmlFor="name">Nom</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="form-control"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
             </div>
-          ))}
-        </Carousel>
-      </section>
+
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="form-control"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <textarea
+                id="message"
+                name="message"
+                className="form-control"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            {error && <p className="text-danger">{error}</p>} {/* Gestion des erreurs */}
+
+            <button type="submit" className="btn btn-primary">
+              Envoyer
+            </button>
+          </form>
+        </div>
+      </div>
+
+      {/* Modal Bootstrap */}
+      <Modal show={submitted} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Merci !</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Merci pour votre message. Je vous contacterai bientôt.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Fermer
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </section>
   );
 };
 
-export default ProjectsComponent;
+export default ContactForm;
